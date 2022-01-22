9842b47204d4fd4cb421c263ecac23a4c26dd92f
hadi499 <hadirpm9@gmail.com>
2022-01-22 12:18:34 +0700

    firt commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const member = {
+    name: 'hadi',
+    message: 'Hello Word.',
+    qty: 1,
+    namaDepan: 'Hadi',
+    namaBelakang: 'Purnomo',
+    bilangan: 4,
+    gambar: 'image/lee.png'
+}
+
+const vm = new Vue({
+    el : '#app',
+    data: member,
+    methods: {
+        getName: function() {
+            return 'hello hadi'
+        },
+        ubahNama: function(namabaru){
+            return this.name = namabaru
+        }
+    },
+    computed:{
+        totalBayar: function(){
+            return this.qty * 1000
+        },
+        namaLengkap: function(){
+            return this.namaDepan + ' ' + this.namaBelakang
+        },
+        infoBilangan: function(){
+            return this.bilangan % 2 === 0 ? 'genap' : 'ganjil'
+        }
+    }
+})
